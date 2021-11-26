1b7aedb6c10ad887a6a6b3d9033d485244659c17
Nadia Mortarino Martinez <56925951+NadiaMortarinoM@users.noreply.github.com>
2021-11-26 17:10:41 -0300

    cambios

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,26 +5,34 @@ import { useNavigation } from "@react-navigation/core"
 export default function Home(){
     return(
         <View style={styles.contenedor}>
-        
-            <Text styles={styles.text}>
-               AppClima 
+            <Text style={styles.textStyle}>
+                Bienvenidos a AppClima el mejor lugar donde saber el clima que tendras en la playa, así poder comer el helado tranquil@!
+                Pensamos está App para aquellos usuarios que quieran disfrutar de las hermosas playas de nuestro país, y ver como estará el clima ese día.
             </Text>
-           
+            <Image style= {styles.imgStyle}
+            source = {require('./img/beach.png')}></Image>
         </View>
-    )
+    );
 }
 
-const styles=StyleSheet.create({
+const styles= StyleSheet.create({
     contenedor:{
-        background:'black',
-        minHeight:'100%'
+        justifyContent: 'center',
+        alignItems: 'center'
+
     },
 
-    text:{
-        color:'blue',
-        fontSize: 20,
+    textStyle:{
+        alignSelf:'center',
+        paddingTop: 10,
+        color: '#2CBAEF',
+        justifyContent:'center',
+        fontSize:25
+    },
+    imgStyle: {
+        width: 200,
+        height: 200,
+        marginTop:30
     }
     
-})
-
-
+});
